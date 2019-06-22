d46381abd9bd6ee271580e527cbfe548d1c99e5b
龚安平 <gonganping@qq.com>
2019-06-22 22:56:56 +0800

    delete code

--- src/KissU.Web/node_modules/@delon/abc/image/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export { ImageDirective } from './image.directive';
-export { ImageConfig } from './image.config';
-export { ImageModule } from './image.module';
--- src/KissU.Web/node_modules/@delon/abc/lodop/public_api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lodop.types';
-export { LodopService } from './lodop.service';
-export { LodopConfig } from './lodop.config';
-export { LodopModule } from './lodop.module';
