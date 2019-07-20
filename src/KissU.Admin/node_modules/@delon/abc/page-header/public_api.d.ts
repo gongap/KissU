8ea0f08c2641da13cf644b826c4adfa76bd89356
Gongap <gonganping@qq.com>
2019-07-20 23:23:29 +0800

    清理代码

--- src/KissU.Admin/node_modules/@delon/abc/page-header/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export { PageHeaderConfig } from './page-header.config';
-export { PageHeaderComponent } from './page-header.component';
-export { PageHeaderModule } from './page-header.module';
--- src/KissU.Admin/node_modules/@delon/abc/zip/public_api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './zip.types';
-export * from './zip.config';
-export { ZipService } from './zip.service';
-export { ZipModule } from './zip.module';
