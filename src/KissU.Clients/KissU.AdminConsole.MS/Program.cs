72ed3d85bbb2f4083b3890cd10b9b304f3dec166
Gongap <gonganping@qq.com>
2019-07-21 16:41:21 +0800

    KissU.Clients

---
+++ src/KissU.Clients/KissU.AdminConsole.MS/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace KissU.AdminConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var ip = config["ip"];
+            var port = config["port"];
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseIISIntegration()
+                .UseStartup<Startup>();
+
+            if (!string.IsNullOrWhiteSpace(ip) && !string.IsNullOrWhiteSpace(port)) host.UseUrls($"http://{ip}:{port}");
+
+            return host;
+        }
+    }
+}
