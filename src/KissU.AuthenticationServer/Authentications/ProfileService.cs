571758054ccd45d50950111f00e07d6c24dcdbeb
Gongap <gonganping@qq.com>
2019-07-21 22:43:24 +0800

    KissU.AuthenticationServer

---
+++ src/KissU.AuthenticationServer/Authentications/ProfileService.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using GreatWall.Domain.Models;
+using GreatWall.Domain.Services.Implements;
+using GreatWall.Service.Abstractions;
+using GreatWall.Service.Dtos;
+using IdentityServer4.AspNetIdentity;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Util;
+using Claim = System.Security.Claims.Claim;
+
+namespace GreatWall.Authentications {
+    /// <summary>
+    /// 用户身份配置服务
+    /// </summary>
+    public class ProfileService : ProfileService<User> {
+        /// <summary>
+        /// 用户声明工厂
+        /// </summary>
+        private readonly IUserClaimsPrincipalFactory<User> _claimsFactory;
+        /// <summary>
+        /// 用户服务
+        /// </summary>
+        private readonly IdentityUserManager _userManager;
+        /// <summary>
+        /// 应用程序服务
+        /// </summary>
+        private readonly IQueryApplicationService _applicationService;
+        /// <summary>
+        /// 角色服务
+        /// </summary>
+        private readonly IRoleService _roleService;
+
+        /// <summary>
+        /// 初始化用户身份配置服务
+        /// </summary>
+        /// <param name="claimsFactory">用户声明工厂</param>
+        /// <param name="userManager">用户服务</param>
+        /// <param name="applicationService">应用程序服务</param>
+        /// <param name="roleService">角色服务</param>
+        public ProfileService( IUserClaimsPrincipalFactory<User> claimsFactory, IdentityUserManager userManager,
+            IQueryApplicationService applicationService, IRoleService roleService ) : base( userManager, claimsFactory ) {
+            _userManager = userManager;
+            _claimsFactory = claimsFactory;
+            _applicationService = applicationService;
+            _roleService = roleService;
+        }
+
+        /// <summary>
+        /// 获取用户配置
+        /// </summary>
+        public override async Task GetProfileDataAsync( ProfileDataRequestContext context ) {
+            if( context == null )
+                return;
+            var user = await GetUser( context );
+            if( user == null )
+                return;
+            var principal = await _claimsFactory.CreateAsync( user );
+            if( principal == null )
+                return;
+            var identity = principal.Identities.First();
+            await AddApplicationClaims( context, identity );
+            await AddRoleClaims( user, identity );
+            context.AddRequestedClaims( principal.Claims );
+        }
+
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        private async Task<User> GetUser( ProfileDataRequestContext context ) {
+            var userId = context.Subject?.GetSubjectId();
+            if( userId.IsEmpty() )
+                return null;
+            return await _userManager.FindByIdAsync( userId );
+        }
+
+        /// <summary>
+        /// 添加应用程序声明
+        /// </summary>
+        protected virtual async Task AddApplicationClaims( ProfileDataRequestContext context, ClaimsIdentity identity ) {
+            var application = await GetApplication( context );
+            if( application == null )
+                return;
+            identity.AddClaim( new Claim( Util.Security.Claims.ClaimTypes.ApplicationId, application.Id ) );
+            identity.AddClaim( new Claim( Util.Security.Claims.ClaimTypes.ApplicationCode, application.Code ) );
+            identity.AddClaim( new Claim( Util.Security.Claims.ClaimTypes.ApplicationName, application.Name ) );
+        }
+
+        /// <summary>
+        /// 获取应用程序
+        /// </summary>
+        private async Task<ApplicationDto> GetApplication( ProfileDataRequestContext context ) {
+            var applicationCode = context.Client?.ClientId;
+            if( applicationCode.IsEmpty() )
+                return null;
+            return await _applicationService.GetByCodeAsync( applicationCode );
+        }
+
+        /// <summary>
+        /// 添加角色声明
+        /// </summary>
+        protected virtual async Task AddRoleClaims( User user, ClaimsIdentity identity ) {
+            var roles = await _roleService.GetRolesAsync( user.Id );
+            if( roles == null || roles.Count == 0 )
+                return;
+            identity.AddClaim( new Claim( Util.Security.Claims.ClaimTypes.RoleIds, roles.Select( t => t.Id ).ToList().Join() ) );
+            identity.AddClaim( new Claim( Util.Security.Claims.ClaimTypes.RoleName, roles.Select( t => t.Name ).ToList().Join() ) );
+        }
+    }
+}
---
+++ src/KissU.AuthenticationServer/Models/LoginViewModel.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using GreatWall.Service.Dtos.Requests;
+using Util;
+
+namespace GreatWall.Models {
+    /// <summary>
+    /// 登录参数
+    /// </summary>
+    public class LoginViewModel {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [Required]
+        public string UserName { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+        /// <summary>
+        /// 记住密码
+        /// </summary>
+        [Display( Name = "记住密码" )]
+        public bool Remember { get; set; }
+        /// <summary>
+        /// 允许记住密码
+        /// </summary>
+        public bool AllowRemember { get; set; } = true;
+        /// <summary>
+        /// 返回地址
+        /// </summary>
+        public string ReturnUrl { get; set; }
+        /// <summary>
+        /// 启用本地登录
+        /// </summary>
+        public bool EnableLocalLogin { get; set; } = true;
+        /// <summary>
+        /// 外部认证提供器列表
+        /// </summary>
+        public IEnumerable<ExternalProvider> ExternalProviders { get; set; } = Enumerable.Empty<ExternalProvider>();
+        /// <summary>
+        /// 是否仅支持外部登录
+        /// </summary>
+        public bool IsExternalLoginOnly => EnableLocalLogin == false && ExternalProviders?.Count() == 1;
+        /// <summary>
+        /// 外部认证方案
+        /// </summary>
+        public string ExternalLoginScheme => IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
+        /// <summary>
+        /// 可显示的外部认证提供器列表
+        /// </summary>
+        public IEnumerable<ExternalProvider> VisibleExternalProviders => ExternalProviders.Where( x => x.DisplayName.IsEmpty() == false );
+        /// <summary>
+        /// 消息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 转换为登录请求
+        /// </summary>
+        public LoginRequest ToLoginRequest() {
+            var result = new LoginRequest {
+                UserName = UserName,
+                Password = Password,
+                Remember = Remember
+            };
+            return result;
+        }
+    }
+}
