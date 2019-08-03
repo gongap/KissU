8cdd9a5e99789041bd196a250091e61ac9f7e055
Gongap <gonganping@qq.com>
2019-08-04 00:13:45 +0800

    修复关卡跨域策略配置问题

--- src/KissU.AdminConsole/ClientAPP/src/environments/environment.ts
+++ src/KissU.AdminConsole/ClientAPP/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: `http://localhost:729`,
+  SERVER_URL: `http://localhost:282`,
   production: false,
   useHash: true,
   hmr: false,
