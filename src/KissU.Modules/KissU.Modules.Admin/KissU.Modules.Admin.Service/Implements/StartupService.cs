27f55ddd5787c3ed5e17b38319cfd35c8522d0b0
龚安平 <gonganping@qq.com>
2019-12-29 00:25:57 +0800

    StartupService

--- src/KissU.Modules/KissU.Modules.Admin/KissU.Modules.Admin.Service/Implements/StartupService.cs
+++ src/KissU.Modules/KissU.Modules.Admin/KissU.Modules.Admin.Service/Implements/StartupService.cs
@@ -21,10 +21,10 @@ namespace KissU.Modules.Admin.Service.Implements
         [HttpGet(true)]
         public async Task<AppData> GetAppDataAsync()
         {
-            object payload = RpcContext.GetContext().GetAttachment("payload");
-            AppData data = new AppData
+            var payload = RpcContext.GetContext().GetAttachment("payload");
+            var data = new AppData
             {
-                App = { Name = "KissU", Description = ".Net Core权限系统" }
+                App = {Name = "KissU", Description = ".Net Core权限系统"}
                 //User = { Name = Web.Identity.Name, Avatar = "/assets/tmp/img/avatar.jpg", Email = "gongap@qq.com" },
                 //Menu = await GetMenus()
             };
@@ -36,7 +36,7 @@ namespace KissU.Modules.Admin.Service.Implements
         /// </summary>
         private async Task<List<MenuInfo>> GetMenus()
         {
-            List<GreatWall.Application.Dtos.Responses.MenuResponse> result = await GetService<IMenuService>().GetMenusAsync();
+            var result = await GetService<IMenuService>().GetMenusAsync();
             return result.ToNgAlainMenus();
         }
     }
