1cc41f033dbd53f889d34369ba93446e1538fbf7
龚安平 <gonganping@qq.com>
2020-02-18 11:33:05 +0800

    ServiceHostTest

--- test/KissU.Core.Tests/KissU.Core.ServiceHosting.Tests/Internal/Implementation/ServiceHostTest.cs
+++ test/KissU.Core.Tests/KissU.Core.ServiceHosting.Tests/Internal/Implementation/ServiceHostTest.cs
@@ -1,4 +1,15 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Autofac;
+using KissU.Core.ServiceHosting.Internal;
+using KissU.Core.ServiceHosting.Internal.Implementation;
+using KissU.Core.ServiceHosting.Startup;
+using KissU.Core.ServiceHosting.Tests.Samples;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Xunit;
 
 namespace KissU.Core.ServiceHosting.Tests.Internal.Implementation
 {
@@ -8,30 +19,26 @@ namespace KissU.Core.ServiceHosting.Tests.Internal.Implementation
     public class ServiceHostTest
     {
         /// <summary>
-        /// Defines the test method TestServiceHost.
-        /// </summary>
-        [Fact()]
-        public void TestServiceHost()
-        {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        /// <summary>
-        /// Defines the test method TestDispose.
-        /// </summary>
-        [Fact()]
-        public void TestDispose()
-        {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        /// <summary>
         /// Defines the test method TestRun.
         /// </summary>
         [Fact()]
-        public void TestRun()
+        public async Task TestRun()
         {
-            Assert.True(false, "This test needs an implementation");
+            var containerBuilder = new ContainerBuilder();
+            var logger = Substitute.For<ILogger<ApplicationLifetime>>();
+            var applicationLifetime = new ApplicationLifetime(logger);
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService<IApplicationLifetime>().Returns(applicationLifetime);
+            var tag = false;
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                tag = true;
+            });
+            var consoleLifetime = new ConsoleLifetime(applicationLifetime);
+            var serviceHost = new ServiceHost(containerBuilder, serviceProvider, consoleLifetime, null);
+            using var host = serviceHost.Run();
+            Assert.True(tag);
+            await serviceHost.StopAsync();
         }
 
         /// <summary>
@@ -40,25 +47,58 @@ namespace KissU.Core.ServiceHosting.Tests.Internal.Implementation
         [Fact()]
         public void TestInitialize()
         {
-            Assert.True(false, "This test needs an implementation");
+            var containerBuilder = new ContainerBuilder();
+            var logger = Substitute.For<ILogger<ApplicationLifetime>>();
+            var startup = Substitute.For<IStartup>();
+            var container = Substitute.For<IContainer>();
+            var applicationLifetime = new ApplicationLifetime(logger);
+            var consoleLifetime = new ConsoleLifetime(applicationLifetime);
+            var services = new ServiceCollection();
+            services.AddSingleton(startup);
+            var serviceProvider = services.BuildServiceProvider();
+            startup.ConfigureServices(containerBuilder).Returns(container);
+            var serviceHost = new ServiceHost(containerBuilder, serviceProvider, consoleLifetime, null);
+            serviceHost.Initialize();
         }
 
         /// <summary>
         /// Defines the test method TestRunAsync.
         /// </summary>
         [Fact()]
-        public void TestRunAsync()
+        public async Task TestRunAsync()
         {
-            Assert.True(false, "This test needs an implementation");
+            var containerBuilder = new ContainerBuilder();
+            var logger = Substitute.For<ILogger<ApplicationLifetime>>();
+            var applicationLifetime = new ApplicationLifetime(logger);
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService<IApplicationLifetime>().Returns(applicationLifetime);
+            var tag = false;
+            applicationLifetime.ApplicationStarted.Register(() => { tag = true; });
+            var consoleLifetime = new ConsoleLifetime(applicationLifetime);
+            var serviceHost = new ServiceHost(containerBuilder, serviceProvider, consoleLifetime, null);
+            await serviceHost.RunAsync();
+            Assert.True(tag);
+            await serviceHost.StopAsync();
         }
 
         /// <summary>
         /// Defines the test method TestStopAsync.
         /// </summary>
         [Fact()]
-        public void TestStopAsync()
+        public async Task TestStopAsync()
         {
-            Assert.True(false, "This test needs an implementation");
+            var containerBuilder = new ContainerBuilder();
+            var logger = Substitute.For<ILogger<ApplicationLifetime>>();
+            var applicationLifetime = new ApplicationLifetime(logger);
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService<IApplicationLifetime>().Returns(applicationLifetime);
+            var tag = false;
+            applicationLifetime.ApplicationStopped.Register(() => { tag = false; });
+            var consoleLifetime = new ConsoleLifetime(applicationLifetime);
+            var serviceHost = new ServiceHost(containerBuilder, serviceProvider, consoleLifetime, null);
+            using var host = serviceHost.Run();
+            await serviceHost.StopAsync();
+            Assert.False(tag);
         }
     }
 }
