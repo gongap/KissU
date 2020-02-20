9713f5109038d268196723d291a29ec4905aec7f
龚安平 <gonganping@qq.com>
2020-02-20 15:22:31 +0800

    添加样例服务主机

---
+++ src/KissU.Microservices/KissU.Services.Hosts/KissU.Services.SampleB.Host/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using Autofac;
+using KissU.Core.Caching.Configurations;
+using KissU.Core.CPlatform;
+using KissU.Core.CPlatform.Configurations;
+using KissU.Core.CPlatform.Utilities;
+using KissU.Core.ProxyGenerator;
+using KissU.Core.ServiceHosting;
+using KissU.Core.ServiceHosting.Internal.Implementation;
+using Microsoft.Extensions.Logging;
+
+namespace KissU.Services.SampleB.Host
+{
+    /// <summary>
+    /// 应用程序
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 应用程序入口点
+        /// </summary>
+        /// <param name="args">入口点参数</param>
+        private static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var host = new ServiceHostBuilder()
+                .RegisterServices(builder =>
+                {
+                    builder.AddMicroService(option =>
+                    {
+                        option.AddServiceRuntime()
+                        .AddRelateService()
+                        .AddConfigurationWatch()
+                        .AddServiceEngine(typeof(ServiceEngine));
+                    });
+                    builder.Register(p => new CPlatformContainer(ServiceLocator.Current));
+                })
+                .ConfigureLogging(logger => { logger.AddConfiguration(AppConfig.GetSection("Logging")); })
+                .UseServer(options => { })
+                .UseConsoleLifetime()
+                .Configure(build => build.AddCacheFile("${cachepath}|cachesettings.json", AppContext.BaseDirectory, false, true))
+                .Configure(build => build.AddCPlatformFile("${kissupath}|servicesettings.json", false, true))
+                .UseStartup<Startup>()
+                .Build();
+
+            using (host.Run())
+            {
+                Console.WriteLine($"服务端启动成功，{DateTime.Now}。");
+            }
+        }
+    }
+}
---
+++ src/KissU.Microservices/KissU.Services.Hosts/KissU.Services.SampleB.Host/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Autofac;
+using KissU.Core.Caching.Configurations;
+using KissU.Core.CPlatform.Utilities;
+using KissU.Core.EventBusKafka.Configurations;
+using KissU.Util;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KissU.Services.SampleB.Host
+{
+    /// <summary>
+    /// 启动配置
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// 初始化启动配置
+        /// </summary>
+        public Startup(IConfigurationBuilder build)
+        {
+            ConfigureEventBus(build);
+            ConfigureCache(build);
+        }
+
+        /// <summary>
+        /// 配置服务
+        /// </summary>
+        public IContainer ConfigureServices(ContainerBuilder builder)
+        {
+            var serivces = new ServiceCollection();
+            return builder.AddUtil(serivces);
+        }
+
+        /// <summary>
+        /// 配置应用
+        /// </summary>
+        public void Configure(IContainer app)
+        {
+            ServiceLocator.Current = app;
+        }
+
+        /// <summary>
+        /// 配置事件总线
+        /// </summary>
+        /// <param name="build">服务构建者</param>
+        private static void ConfigureEventBus(IConfigurationBuilder build)
+        {
+            build.AddEventBusFile("eventbussettings.json", false);
+        }
+
+        /// <summary>
+        /// 配置缓存服务
+        /// </summary>
+        private void ConfigureCache(IConfigurationBuilder build)
+        {
+            build.AddCacheFile("cachesettings.json", false);
+        }
+    }
+}
