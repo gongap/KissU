075148fc56a445d186a27a08d5b14bc7a69e224c
龚安平 <gonganping@qq.com>
2020-03-22 16:18:34 +0800

    dist

--- src/KissU.Clients/KissU.AdminConsole/Startup.cs
+++ src/KissU.Clients/KissU.AdminConsole/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +20,11 @@ namespace KissU.AdminConsole
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "Dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +43,10 @@ namespace KissU.AdminConsole
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            //if (!env.IsDevelopment())
+            //{
+                app.UseSpaStaticFiles();
+            //}
 
             app.UseRouting();
 
@@ -45,6 +55,19 @@ namespace KissU.AdminConsole
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "App";
+
+                //if (env.IsDevelopment())
+                //{
+                //    spa.UseAngularCliServer(npmScript: "start");
+                //}
             });
         }
     }
