d3c56ad6b8bad4f98f2c7704792176d24f2c9dbc
龚安平 <gonganping@qq.com>
2020-04-08 16:47:25 +0800

    修改Module名称

---
+++ src/KissU.BuildingBlocks/KissU.Util/KissU.Util.Ddd.Domain.Datas/UtilDomainDatasModule.cs
@@ -0,0 +1,13 @@
+﻿using KissU.Core.Module;
+
+namespace KissU.Util.Ddd.Domain.Datas
+{
+    /// <summary>
+    /// UtilDatasModule.
+    /// Implements the <see cref="UtilDomainDatasModule" />
+    /// </summary>
+    /// <seealso cref="UtilDomainDatasModule" />
+    public class UtilDomainDatasModule : SystemModule
+    {
+    }
+}
---
+++ src/KissU.BuildingBlocks/KissU.Util/KissU.Util.Ddd.Domain/UtilDddDomainModule.cs
@@ -0,0 +1,13 @@
+﻿using KissU.Core.Module;
+
+namespace KissU.Util.Ddd.Domain
+{
+    /// <summary>
+    /// UtilDddModule.
+    /// Implements the <see cref="UtilDddDomainModule" />
+    /// </summary>
+    /// <seealso cref="UtilDddDomainModule" />
+    public class UtilDddDomainModule : SystemModule
+    {
+    }
+}
