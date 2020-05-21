c56b2058adc0b3ef774fbff555afd689233b888a
龚安平 <gonganping@qq.com>
2020-05-21 20:52:32 +0800

    temp

---
+++ microservice/KissU.Modules/KissU.Modules.Identity/src/KissU.Modules.Identity.Service/AbpIdentityModule.cs
@@ -0,0 +1,37 @@
+﻿using Localization.Resources.AbpUi;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Identity.EntityFrameworkCore;
+using Volo.Abp.Identity.Localization;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using KissU.Abp.Autofac;
+
+namespace Volo.Abp.Identity.Service
+{
+    [DependsOn(
+        typeof(AbpIdentityApplicationModule),
+        typeof(AbpIdentityEntityFrameworkCoreModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class AbpIdentityModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpDbContextOptions>(options =>
+            {
+                /* The main point to change your DBMS.
+                 * See also BookStoreMigrationsDbContextFactory for EF Core tooling. */
+                options.UseSqlServer();
+            }); 
+            
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Resources
+                    .Get<IdentityResource>()
+                    .AddBaseTypes(
+                        typeof(AbpUiResource)
+                    );
+            });
+        }
+    }
+}
---
+++ microservice/KissU.Modules/KissU.Modules.Identity/src/KissU.Modules.Identity.Service/IdentityModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Autofac.Extensions.DependencyInjection;
+using KissU.Core.Module;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Volo.Abp.Identity.Service
+{
+    public class IdentityModule : BusinessModule
+    {
+        private IAbpApplicationWithExternalServiceProvider application;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="moduleContext">应用模块上下文</param>
+        public override void Initialize(AppModuleContext moduleContext)
+        {
+            base.Initialize(moduleContext);
+            var serviceProvoider = moduleContext.ServiceProvoider.GetInstances<IServiceProvider>();
+            application.Initialize(serviceProvoider);
+        }
+
+        /// <summary>
+        /// 注册第三方组件
+        /// </summary>
+        /// <param name="builder">容器构建器</param>
+        protected override void RegisterBuilder(ContainerBuilderWrapper builder)
+        {
+            var services = new ServiceCollection();
+            services.AddObjectAccessor<AbpIdentityModule>();
+            application = AbpApplicationFactory.Create<AbpIdentityModule>(services);
+            builder.ContainerBuilder.Populate(services);
+        }
+    }
+}
