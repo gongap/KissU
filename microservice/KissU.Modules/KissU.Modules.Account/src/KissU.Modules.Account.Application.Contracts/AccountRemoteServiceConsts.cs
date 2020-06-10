da06f3484f2b7e02c4d7b14bef06bc5e6ece60bd
龚安平 <gonganping@qq.com>
2020-06-10 12:11:59 +0800

    delete Files

--- microservice/KissU.Modules/KissU.Modules.Account/src/KissU.Modules.Account.Application.Contracts/AccountRemoteServiceConsts.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KissU.Modules.Account.Application.Contracts
-{
-    public static class AccountRemoteServiceConsts
-    {
-        public const string RemoteServiceName = "AbpAccount";
-    }
-}
