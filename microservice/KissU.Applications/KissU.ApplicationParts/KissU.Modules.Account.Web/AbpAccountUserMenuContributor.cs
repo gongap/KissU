bfe8b0cf5fe092269a699e296e3c69f86fe11ce0
龚安平 <gonganping@qq.com>
2020-07-14 12:58:06 +0800

    temp

---
+++ microservice/KissU.Applications/KissU.ApplicationParts/KissU.Modules.Account.Web/AbpAccountUserMenuContributor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using KissU.Modules.Account.Application.Contracts.Localization;
+using Localization.Resources.AbpUi;
+using Volo.Abp.UI.Navigation;
+
+namespace KissU.Modules.Account.Web
+{
+    public class AbpAccountUserMenuContributor : IMenuContributor
+    {
+        public virtual Task ConfigureMenuAsync(MenuConfigurationContext context)
+        {
+            if (context.Menu.Name != StandardMenus.User)
+            {
+                return Task.CompletedTask;
+            }
+
+            var uiResource = context.GetLocalizer<AbpUiResource>();
+            var accountResource = context.GetLocalizer<AccountResource>();
+
+            context.Menu.AddItem(new ApplicationMenuItem("Account.Manage", accountResource["ManageYourProfile"], url: "~/Account/Manage", icon: "fa fa-cog", order: 1000, null));
+            context.Menu.AddItem(new ApplicationMenuItem("Account.Logout", uiResource["Logout"], url: "~/Account/Logout", icon: "fa fa-power-off", order: int.MaxValue - 1000));
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ microservice/KissU.Applications/KissU.ApplicationParts/KissU.Modules.Account.Web/AbpAccountWebAutomapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using KissU.Modules.Account.Web.Pages.Account;
+using KissU.Modules.Identity.Application.Contracts;
+
+namespace KissU.Modules.Account.Web
+{
+    public class AbpAccountWebAutoMapperProfile : Profile
+    {
+        public AbpAccountWebAutoMapperProfile()
+        {
+            CreateMap<ProfileDto, PersonalSettingsInfoModel>();
+        }
+    }
+}
