c209e28b3b016edf67ae1d5d60425a961e85bf8b
gongap <gonganping@qq.com>
2021-03-09 11:31:56 +0800

    授权类型

--- framework/src/KissU.CPlatform/Filters/Implementation/AuthorizationType.cs
+++ framework/src/KissU.CPlatform/Filters/Implementation/AuthorizationType.cs
@@ -5,19 +5,8 @@
     /// </summary>
     public enum AuthorizationType
     {
-        /// <summary>
-        /// The JWT
-        /// </summary>
         JWT,
-
-        /// <summary>
-        /// The application secret
-        /// </summary>
         AppSecret,
-
-        /// <summary>
-        /// The JWT bearer
-        /// </summary>
-        JWTBearer
+        Bearer
     }
 }
