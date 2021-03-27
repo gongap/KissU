a83672c2e95c78aa898f8ddf5d78f92dbb65b9d0
龚安平 <gonganping@qq.com>
2021-03-27 22:59:19 +0800

    common

---
+++ modules/common/KissU.Modules.Common.Service.Contracts/CommonServiceContractsModule.cs
@@ -0,0 +1,13 @@
+﻿using KissU.Modularity;
+using Volo.Abp.Modularity;
+
+namespace KissU.Modules.Account.Service.Contracts
+{
+    [DependsOn]
+    public class CommonServiceContractsModule : AbpModule, IBusinessModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+    }
+}
---
+++ modules/common/KissU.Modules.Common.Service/CommonServiceModule.cs
@@ -0,0 +1,16 @@
+﻿using KissU.Modularity;
+using KissU.Modules.Account.Service.Contracts;
+using Volo.Abp.Modularity;
+
+namespace KissU.Modules.Account.Service
+{
+    [DependsOn(
+        typeof(CommonServiceContractsModule)
+        )]
+    public class CommonServiceModule : AbpModule, IBusinessModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+    }
+}
