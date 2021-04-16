a7c3782e0faefaf44109d6c958486a73a3a4f3dd
龚安平 <gonganping@qq.com>
2021-04-16 09:32:54 +0800

    CultureInfo

--- framework/src/KissU.Abp/AbpStartupModule.cs
+++ framework/src/KissU.Abp/AbpStartupModule.cs
@@ -1,15 +1,26 @@
-﻿using Volo.Abp.Autofac;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using System.Collections.Generic;
+using System.Globalization;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
-using Volo.Abp.Security;
 
 namespace KissU.Abp
 {
     [DependsOn(typeof(AbpAutofacModule)
-        ,typeof(AbpSecurityModule))]
+        , typeof(AbpAspNetCoreModule))]
     public class AbpStartupModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            var cultures = new List<CultureInfo> { new CultureInfo("zh-Hans"), new CultureInfo("en") };
+            Configure<RequestLocalizationOptions>(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture("zh-Hans");
+                options.SupportedCultures = cultures;
+                options.SupportedUICultures = cultures;
+            });
         }
     }
 }
