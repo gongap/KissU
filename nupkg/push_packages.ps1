6c6afd44e78cd20420383dbca0d30f0c6458be41
龚安平 <gonganping@qq.com>
2021-10-16 17:23:14 +0800

    push_packages.ps1

--- nupkg/push_packages.ps1
+++ nupkg/push_packages.ps1
@@ -6,6 +6,9 @@ $apiKey = $args[0]
 [xml]$commonPropsXml = Get-Content (Join-Path $rootFolder "common.props")
 $version = $commonPropsXml.Project.PropertyGroup.Version
 
+# Go back to the pack folder
+Set-Location $packFolder
+
 # Publish all packages
 foreach($project in $projects) {
     $projectName = $project.Substring($project.LastIndexOf("/") + 1)
